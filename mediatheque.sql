bd8bbc697707533a0b630c2bea3d15dde190c670
Yacine <guerda.yacine60100@gmail.com>
2025-09-25 23:40:03 +0200

    modif du sql

--- mediatheque.sql
+++ mediatheque.sql
@@ -1,48 +1,285 @@
-CREATE DATABASE mediatheque DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-USE mediatheque;
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mediatheque
+-- ------------------------------------------------------
+-- Server version	8.0.41
 
-CREATE TABLE users (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+;
+/*!50503 SET NAMES utf8 */
+;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */
+;
+/*!40103 SET TIME_ZONE='+00:00' */
+;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */
+;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */
+;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */
+;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */
+;
 
-CREATE TABLE media (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    available BOOLEAN NOT NULL DEFAULT TRUE,
-    media_type ENUM('book','album','movie') NOT NULL
-);
+--
+-- Table structure for table `album`
+--
 
-CREATE TABLE book (
-    id INT UNSIGNED PRIMARY KEY,
-    page_number INT NOT NULL,
-    FOREIGN KEY (id) REFERENCES media(id) ON DELETE CASCADE
-);
+DROP TABLE IF EXISTS `album`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+;
+/*!50503 SET character_set_client = utf8mb4 */
+;
+CREATE TABLE `album` (
+    `id` int unsigned NOT NULL,
+    `editor` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `album_ibfk_1` FOREIGN KEY (`id`) REFERENCES `media` (`id`) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
 
-CREATE TABLE movie (
-    id INT UNSIGNED PRIMARY KEY,
-    duration DOUBLE NOT NULL,
-    genre ENUM('Action','Comédie','Drame','Horreur','Autre') NOT NULL,
-    FOREIGN KEY (id) REFERENCES media(id) ON DELETE CASCADE
-);
+--
+-- Dumping data for table `album`
+--
 
-CREATE TABLE album (
-    id INT UNSIGNED PRIMARY KEY,
-    track_number INT NOT NULL,
-    editor VARCHAR(255) NOT NULL,
-    FOREIGN KEY (id) REFERENCES media(id) ON DELETE CASCADE
-);
+LOCK TABLES `album` WRITE;
+/*!40000 ALTER TABLE `album` DISABLE KEYS */
+;
+/*!40000 ALTER TABLE `album` ENABLE KEYS */
+;
+UNLOCK TABLES;
 
-CREATE TABLE song (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    album_id INT UNSIGNED NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    duration FLOAT NOT NULL,
-    note INT UNSIGNED NOT NULL,
-    FOREIGN KEY (album_id) REFERENCES album(id) ON DELETE CASCADE
-);
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+;
+/*!50503 SET character_set_client = utf8mb4 */
+;
+CREATE TABLE `book` (
+    `id` int unsigned NOT NULL,
+    `page_number` int NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `book_ibfk_1` FOREIGN KEY (`id`) REFERENCES `media` (`id`) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */
+;
+/*!40000 ALTER TABLE `book` ENABLE KEYS */
+;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `location`
+--
+
+DROP TABLE IF EXISTS `location`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+;
+/*!50503 SET character_set_client = utf8mb4 */
+;
+CREATE TABLE `location` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `user_id` int unsigned NOT NULL,
+    `media_id` int unsigned NOT NULL,
+    `borrowed_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    `returned_at` timestamp NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `user_id` (`user_id`),
+    KEY `media_id` (`media_id`),
+    CONSTRAINT `location_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+    CONSTRAINT `location_ibfk_2` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
+
+--
+-- Dumping data for table `location`
+--
+
+LOCK TABLES `location` WRITE;
+/*!40000 ALTER TABLE `location` DISABLE KEYS */
+;
+/*!40000 ALTER TABLE `location` ENABLE KEYS */
+;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `media`
+--
+
+DROP TABLE IF EXISTS `media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+;
+/*!50503 SET character_set_client = utf8mb4 */
+;
+CREATE TABLE `media` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `author` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `available` tinyint NOT NULL,
+    `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `media_type` enum('book', 'album', 'movie') COLLATE utf8mb4_unicode_ci NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 42 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
+
+--
+-- Dumping data for table `media`
+--
+
+LOCK TABLES `media` WRITE;
+/*!40000 ALTER TABLE `media` DISABLE KEYS */
+;
+/*!40000 ALTER TABLE `media` ENABLE KEYS */
+;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movie`
+--
+
+DROP TABLE IF EXISTS `movie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+;
+/*!50503 SET character_set_client = utf8mb4 */
+;
+CREATE TABLE `movie` (
+    `id` int unsigned NOT NULL,
+    `duration` double NOT NULL,
+    `genre` enum(
+        'Action',
+        'Comédie',
+        'Drame',
+        'Horreur',
+        'Autre'
+    ) COLLATE utf8mb4_unicode_ci NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `movie_ibfk_1` FOREIGN KEY (`id`) REFERENCES `media` (`id`) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
+
+--
+-- Dumping data for table `movie`
+--
+
+LOCK TABLES `movie` WRITE;
+/*!40000 ALTER TABLE `movie` DISABLE KEYS */
+;
+/*!40000 ALTER TABLE `movie` ENABLE KEYS */
+;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `song`
+--
+
+DROP TABLE IF EXISTS `song`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+;
+/*!50503 SET character_set_client = utf8mb4 */
+;
+CREATE TABLE `song` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `album_id` int unsigned DEFAULT NULL,
+    `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `author` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `available` tinyint(1) NOT NULL,
+    `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `duration` float NOT NULL,
+    `note` int unsigned NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `song_ibfk_1` (`album_id`),
+    CONSTRAINT `song_ibfk_1` FOREIGN KEY (`album_id`) REFERENCES `album` (`id`) ON DELETE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
+
+--
+-- Dumping data for table `song`
+--
+
+LOCK TABLES `song` WRITE;
+/*!40000 ALTER TABLE `song` DISABLE KEYS */
+;
+/*!40000 ALTER TABLE `song` ENABLE KEYS */
+;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+;
+/*!50503 SET character_set_client = utf8mb4 */
+;
+CREATE TABLE `users` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `email` (`email`)
+) ENGINE = InnoDB AUTO_INCREMENT = 4 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */
+;
+INSERT INTO
+    `users`
+VALUES (
+        3,
+        'yexouwu',
+        'guerda.yacine60100@gmail.com',
+        '$argon2id$v=19$m=65536,t=4,p=1$ckpZMS9DVzNPRzQyRXZ0cw$h4vy88Q3nwNqztYo00KmsyzhCV4KAWwysNjmlcJ7Tvo',
+        '2025-09-20 13:09:29',
+        NULL
+    );
+/*!40000 ALTER TABLE `users` ENABLE KEYS */
+;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */
+;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */
+;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */
+;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */
+;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
+;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */
+;
+
+-- Dump completed on 2025-09-25 23:36:46
